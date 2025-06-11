96c5c2c6bbf517d48d853757a850bdc80aab53e7
grady <grady0902@gmail.com>
2025-06-11 15:09:23 +0800

    refactor: 元件化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { FeatureProjectsComponent } from './feature-projects/feature-projects.component';
+import { TechStackComponent } from './tech-stack/tech-stack.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    HeaderComponent, 
+    AboutComponent, AchievementsComponent, ExperienceComponent, 
+    TechStackComponent, FeatureProjectsComponent, ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/feature-projects/feature-projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-projects',
+  imports: [],
+  templateUrl: './feature-projects.component.html',
+  styleUrl: './feature-projects.component.scss'
+})
+export class FeatureProjectsComponent {
+
+}
