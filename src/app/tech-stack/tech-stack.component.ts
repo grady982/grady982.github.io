33e9b4f8355bbacd4d5dedd6677717a261ed3f83
grady <grady0902@gmail.com>
2025-06-19 15:28:44 +0800

    refactor: tech stack component to use dynamic skill list

--- src/app/tech-stack/tech-stack.component.ts
+++ src/app/tech-stack/tech-stack.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './tech-stack.component.scss'
 })
 export class TechStackComponent {
-
+  stackList: string[] = [
+    'Angular',
+    'React',
+    'Scss',
+    'Jasmine',
+    'karma',
+    'Selenium',
+    'Redux',
+    'NgRx',
+    'TypeScript',
+    'JavaScript',
+    'NodeJs',
+    'Express.js',
+    'Koa.js',
+    'pm2',
+    'Java',
+    'Spring Boot',
+    'MSSQL',
+    'PostgreSQL',
+    'Docker',
+    'Kubernetes',
+  ];
 }
