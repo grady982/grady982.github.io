701fa8766708a7e5d6910da187af1ed6381aa7f5
Grady <grady0902@gmail.com>
2025-12-08 23:38:40 +0800

    feat: add responsive hamburger menu to header with navigation links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,16 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  onMenuItemClick(item: string): void {
+    window.location.href = `/home#${item}`;
+  }
 }
